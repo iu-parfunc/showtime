78d1aacc656fb83029deb321a77738a807ba6fcd
Ryan Scott <ryan.gl.scott@gmail.com>
2019-05-15 15:11:01 -0400

    mmaps dependency

--- coq/default.nix
+++ coq/default.nix
@@ -1,6 +1,9 @@
 # Adapted from https://github.com/coq-community/manifesto/wiki/Continuous-Integration-with-Nix/ed232957a8e84a680ded4f96edaf3b9a8f17326f#simpler-setup-when-not-testing-coqs-development-branch
 
-{ pkgs ? (import <nixpkgs> {}), shell ? false }:
+{ pkgs ? (import <nixpkgs> {})
+, mmaps ? import ./deps/mmaps/default.nix {}
+, shell ? false
+}:
 
 with pkgs.coqPackages_8_9;
 
@@ -10,6 +13,7 @@ pkgs.stdenv.mkDerivation {
 
   propagatedBuildInputs = [
     coq
+    mmaps
   ];
 
   src = if shell then null else ./.;
