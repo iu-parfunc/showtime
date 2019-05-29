3829838c9cc7f69ba343681ff7b0407e5505086b
Ryan Scott <ryan.gl.scott@gmail.com>
2019-05-29 15:13:08 -0400

    Define the showtimes function

--- coq/Showtime.v
+++ coq/Showtime.v
@@ -1,5 +1,5 @@
 From Showtime Require Import Error Max MSet State.
-Require Import List String.
+Require Import Arith.Wf_nat Bool List Omega.
 Require Import Structures.OrdersEx.
 Open Scope string_scope.
 
@@ -54,22 +54,163 @@ Definition dropWhile {a} (p : a -> bool) : list a -> list a :=
            else xs
     end.
 
-(*
-TODO: How to convince Coq that this terminates?
+Lemma induction_ltof1_Prop
+     : forall (A : Set) (f : A -> nat) (P : A -> Prop),
+       (forall x : A, (forall y : A, ltof A f y x -> P y) -> P x) ->
+       forall a : A, P a.
+Proof.
+  intros A f P F.
+  assert (H : forall (n : nat) (a : A), (f a < n)%nat -> P a).
+  { induction n; intros.
+    - omega.
+    - apply F. unfold ltof. intros. apply IHn.
+      apply Nat.lt_le_trans with (m := f a); omega. }
+  intros. apply H with (n := S (f a)). omega.
+Defined.
 
-Fixpoint open (ls : list Time) {struct ls} : list Time :=
-  match ls with
-  | nil       => nil
-  | t0 :: rst => t0 :: closed (t0 + showLen) rst
-  end
+Definition flip_ltb (x : Max) : Max -> bool :=
+  fun (y : Max) => y <? x.
 
-  with
-    closed (expiry : Time) (ls : list Time) {struct ls} : list Time :=
-      match ls with
-      | nil       => nil
-      | t1 :: rst =>
+Inductive
+  OpenAcc : list Time -> Prop :=
+  | OpenAccNil  : OpenAcc nil
+  | OpenAccCons : forall t0 rst,
+                  ClosedAcc (t0 + showLen) rst ->
+                  OpenAcc (t0 :: rst)
+with
+  ClosedAcc : Time -> list Time -> Prop :=
+  | ClosedAccNil  : forall expiry, ClosedAcc expiry nil
+  | ClosedAccCons : forall expiry t1 rst,
+                    ClosedAcc (expiry + showLen) (dropWhile (flip_ltb expiry) rst) ->
+                    OpenAcc (t1 :: rst) ->
+                    ClosedAcc expiry (t1 :: rst).
+
+Lemma OpenAcc_inv :
+  forall {l : list Time} {t0 : Time} {rst : list Time},
+  OpenAcc l -> l = (t0 :: rst) ->
+  ClosedAcc (t0 + showLen) rst.
+Proof. intros. subst. inversion H. auto. Defined.
+
+Lemma ClosedAcc_inv_1 :
+  forall {expiry : Time} {ls : list Time} {t1 : Time} {rst : list Time},
+  ClosedAcc expiry ls -> ls = (t1 :: rst) ->
+  ClosedAcc (expiry + showLen) (dropWhile (flip_ltb expiry) rst).
+Proof. intros. subst. inversion H. auto. Defined.
+
+Lemma ClosedAcc_inv_2 :
+  forall {expiry : Time} {ls : list Time} {t1 : Time} {rst : list Time},
+  ClosedAcc expiry ls -> ls = (t1 :: rst) ->
+  OpenAcc (t1 :: rst).
+Proof. intros. subst. inversion H. auto. Defined.
+
+Fixpoint
+  open' (ls : list Time) (acc : OpenAcc ls) {struct acc} : list Time :=
+    match ls as ls0 return (ls = ls0 -> list Time) with
+    | nil =>
+        fun (_ : ls = nil) => nil
+    | t0 :: rst =>
+        fun (H : ls = (t0 :: rst)) =>
+        t0 :: closed' (t0 + showLen) rst (OpenAcc_inv acc H)
+    end (eq_refl ls)
+with
+  closed' (expiry : Time) (ls : list Time)
+          (acc : ClosedAcc expiry ls) {struct acc} : list Time :=
+    match ls as ls0 return (ls = ls0 -> list Time) with
+    | nil =>
+        fun (_ : ls = nil) => nil
+    | t1 :: rst =>
+        fun (H : ls = (t1 :: rst)) =>
         if t1 <? expiry
-           then expiry :: closed (expiry + showLen) (dropWhile (fun x => x <? expiry) rst)
-           else open (t1 :: rst)
-      end.
+        then expiry :: closed' (expiry + showLen)
+                               (dropWhile (flip_ltb expiry) rst)
+                               (ClosedAcc_inv_1 acc H)
+        else open' (t1 :: rst) (ClosedAcc_inv_2 acc H)
+    end (eq_refl ls).
+
+Lemma dropWhile_length :
+  forall {a} (f : a -> bool) (l : list a),
+  (length (dropWhile f l) <= length l)%nat.
+Proof.
+  induction l; simpl; auto.
+  destruct (f a0); simpl; auto.
+Defined.
+
+Theorem
+  openAcc : forall (ls : list Time), OpenAcc ls
+with
+  closedAcc : forall (expiry : Time) (ls : list Time), ClosedAcc expiry ls.
+Proof.
+- induction ls; constructor. apply closedAcc.
+- intros. change (ClosedAcc expiry ls) with (prod_curry ClosedAcc (expiry, ls)).
+  apply (induction_ltof1_Prop (Time * list Time)
+                              (prod_curry (fun _ => @length Time))
+                              (prod_curry ClosedAcc)).
+  intros. destruct x. destruct l as [|x xs]; constructor.
+  + change (ClosedAcc (t + showLen) (dropWhile (flip_ltb t) xs))
+      with (prod_curry ClosedAcc (t + showLen, (dropWhile (flip_ltb t) xs))).
+    apply H. unfold ltof, prod_curry.
+    pose proof (dropWhile_length (flip_ltb t) xs). simpl.
+    apply le_lt_n_Sm. auto.
+  + constructor.
+    change (ClosedAcc (x + showLen) xs)
+      with (prod_curry ClosedAcc (x + showLen, xs)).
+    apply H. unfold ltof, prod_curry. simpl. omega.
+Defined.
+Scheme OpenAcc_ind_dep   := Induction for OpenAcc   Sort Prop.
+Scheme ClosedAcc_ind_dep := Induction for ClosedAcc Sort Prop.
+
+Definition open (ls : list Time) : list Time :=
+  open' ls (openAcc ls).
+Definition closed (expiry : Time) (ls : list Time) : list Time :=
+  closed' expiry ls (closedAcc expiry ls).
+Definition showtimes : list Time -> list Time := open.
+
+(*
+Lemma
+  open'_PI' :
+    forall {l1} (acc1 : OpenAcc l1) {l2} (acc2 : OpenAcc l2),
+    l1 = l2 ->
+    open' l1 acc1 = open' l2 acc2
+with
+  closed'_PI' :
+    forall {expiry1 l1} (acc1 : ClosedAcc expiry1 l1)
+           {expiry2 l2} (acc2 : ClosedAcc expiry2 l2),
+    expiry1 = expiry2 -> l1 = l2 ->
+    closed' expiry1 l1 acc1 = closed' expiry2 l2 acc2.
+Proof.
+- apply (OpenAcc_ind_dep
+        (fun (l' : list Time) (acc' : OpenAcc l') =>
+         forall (l2 : list Time) (acc2 : OpenAcc l2),
+         l' = l2 -> open' l' acc' = open' l2 acc2)).
+  + apply (OpenAcc_ind_dep
+          (fun (l' : list Time) (acc' : OpenAcc l') =>
+           nil = l' -> open' nil OpenAccNil = open' l' acc'));
+    try discriminate. auto.
+  + intros t0 rst c.
+    apply (OpenAcc_ind_dep
+          (fun (l' : list Time) (acc' : OpenAcc l') =>
+           t0 :: rst = l' -> open' (t0 :: rst) (OpenAccCons t0 rst c) = open' l' acc'));
+    try discriminate. intros.
+    injection H as H1 H2. subst. simpl.
+    rewrite (closed'_PI' (t1 + showLen) rst0 (OpenAcc_inv (OpenAccCons t1 rst0 c)  eq_refl)
+                         (t1 + showLen) rst0 (OpenAcc_inv (OpenAccCons t1 rst0 c0) eq_refl));
+    auto.
+- apply (ClosedAcc_ind_dep
+        (fun (expiry' : Time) (l' : list Time) (acc' : ClosedAcc expiry' l') =>
+         forall (expiry2 : Time) (l2 : list Time) (acc2 : ClosedAcc expiry2 l2),
+         expiry' = expiry2 -> l' = l2 ->
+         closed' expiry' l' acc' = closed' expiry2 l2 acc2)).
+  + intro expiry.
+    apply (ClosedAcc_ind_dep
+          (fun (expiry' : Time) (l' : list Time) (acc' : ClosedAcc expiry' l') =>
+           expiry = expiry' -> nil = l' ->
+           closed' expiry nil (ClosedAccNil expiry) = closed' expiry' l' acc'));
+    try discriminate. auto.
+  + intros expiry t1 rst c H o.
+    apply (ClosedAcc_ind_dep
+          (fun (expiry' : Time) (l' : list Time) (acc' : ClosedAcc expiry' l') =>
+           expiry = expiry' -> t1 :: rst = l' ->
+           closed' expiry (t1 :: rst) (ClosedAccCons expiry t1 rst c o) =
+           closed' expiry' l' acc')); try discriminate. intros.
+    injection H2 as H3 H4. subst. simpl. bdestruct (t0 <? expiry0).
 *)
